297643e2aca5c56c78ade2d380183c539e56ca36
James Starkman <8@case.edu>
2016-09-27 14:11:33 -0400

    Added Arduino (requires certain amount of manual fixing), added new global directory for the Android SDK, and added -y switch to upgrade task.

--- choco-install.ps1
+++ choco-install.ps1
@@ -1,11 +1,12 @@
-
-
+﻿
 # Commented entries were broken.  Might be fixed.
 
 choco install -y chocolateygui
 choco install -y git
 choco install -y firefox
 choco install -y notepadplusplus
+choco install -y emacs # might get 64-bit 25.1 soon, but 24.5 for now
+choco install -y sublimetext3
 choco install -y classic-shell
 choco install -y putty
 choco install -y 7zip.install
@@ -16,12 +17,20 @@ choco install -y python # v3.x
 choco install -y miktex
 choco install -y texstudio
 
+# manual config may be needed to get SDK
+Try { mkdir C:\AndroidSDK\ -ea Stop } Catch { echo "Have you run this script before?" }
 choco install -y androidstudio -params '"/PinnedToTaskbar:true"'
 choco install -y adb
-# choco install -y arduino
+echo "Remember to put the SDK in the shared directory."
+
+# Uses AHK to trigger installing drivers for USB (manual may be requried)
+choco install -y arduino --allow-empty-checksums
+
+# Broken on our systems
 # choco install -y visualstudio2015community
 
-$A = New-ScheduledTaskAction –Execute "choco upgrade all"
+# FIXME correct the password before running this file
+$A = New-ScheduledTaskAction –Execute "choco upgrade all -y"
 $T = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Sunday -At 4am
 $S = New-ScheduledTaskSettingsSet
 $D = New-ScheduledTask -Action $A -Trigger $T -Settings $S
